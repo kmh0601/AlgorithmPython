4c34f0580f5424ce29089b5329d8c37ded56674b
kmh0601 <pongkmh@naver.com>
2021-06-09 23:05:55 +0900

    2021.06.09

--- DFS_BFS/DFS_1.py
+++ DFS_BFS/DFS_1.py
@@ -1,1 +1,28 @@
-print('DFS')
+N,M = map(int,input().split())
+
+def DFS(visited,x,y,ice):
+    if ice[x][y] != 1 and visited[x][y] != True:
+        visited[x][y] = True
+        # 오른쪽으로 먼저 탐색
+        if y+1 < M:
+           DFS(visited,x,y+1,ice)
+        # 아래쪽으로 탐색
+        if x+1 < N:
+            DFS(visited,x+1,y,ice)
+
+
+Visited = [[False for _ in range(M)]for _ in range(N)]
+Ice = []
+count = 0
+i = 0
+while i < N:
+    Ice.append(list(map(int,input())))
+    i += 1
+
+
+for x in range(N):
+    for y in range(M):
+        if Ice[x][y] != 1 and Visited[x][y] != True:
+            DFS(Visited,x,y,Ice)
+            count += 1
+print(count)
--- 구현/Implement_4.py
+++ 구현/Implement_4.py
@@ -1,15 +1,15 @@
-str = list(input())
+str_ = list(input())
 count = 0
 new_str = []
 
-for i in str:
+for i in str_:
     if i.isalpha() == False:
         count += int(i)
     else:
         new_str.append(i)
 
 new_str.sort()
-new_str.append(count)
+if count != 0:
+    new_str.append(str(count)  )
 
-for i in new_str:
-     print(i, end ='')
+print("".join(new_str))
